e66823f0e81ff0ad3df0d24b71edf73dcefa8e7c
Alejandro Gomez <agomez302@gatech.edu>
2024-12-22 00:56:46 -0500

    fixes sys path to run

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 from app import app
+import sys
+sys.path.insert(0, "app")
+sys.path.insert("app/models")
+sys.path.insert("app/services")
+sys.path.insert("app/utils")
 
 if __name__ == "__main__":
     app.main()
